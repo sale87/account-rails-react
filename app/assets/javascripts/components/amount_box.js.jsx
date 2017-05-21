cd9e7034005dee96c67f45380a3fd439fbfb0784
Sasa Ranisavljevic <sasa.ranisavljevic@gmail.com>
2017-05-21 21:03:31 +0200

    fixing return

--- app/assets/javascripts/components/amount_box.js.jsx
+++ app/assets/javascripts/components/amount_box.js.jsx
@@ -1,12 +1,14 @@
 var AmountBox = React.createClass({
   render() {
-    <div className="col-md-4">
-      <div className="panel panel-{props.type}">
-        <div className="panel-heading">
-          {props.text}
+    return (
+      <div className="col-md-4">
+        <div className="panel panel-{props.type}">
+          <div className="panel-heading">
+            {props.text}
+          </div>
+        <div className="panel-body">{props.amount}</div>
         </div>
-      <div className="panel-body">{props.amount}</div>
-      </div>  
-    </div>
+      </div>      
+    )
   }
 })
